4f1ed915a00c37079536cb50b5b30db7a3732f4b
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2023-02-15 13:30:24 +0100

    er

--- app/src/main/java/com/example/pmd_persistenciamemoriainterna/MainActivity.java
+++ app/src/main/java/com/example/pmd_persistenciamemoriainterna/MainActivity.java
@@ -3,10 +3,17 @@ package com.example.pmd_persistenciamemoriainterna;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +26,28 @@ public class MainActivity extends AppCompatActivity {
         Button BGuardar = (Button) findViewById(R.id.Bguardar);
         Button BRecuperar = (Button) findViewById(R.id.Brecuperar);
         TextView mostrarTexto = (TextView) findViewById(R.id.showtextView);
+        String sdEstado = Environment.getExternalStorageState();
+
 
         BGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(sdEstado.equals(Environment.MEDIA_MOUNTED){
 
+                    try {
+                        File rutaSD = Environment.getExternalStorageDirectory();
+                        File miFichero = new File(rutaSD.getAbsolutePath(),"pruebaSD.txt");
+                        OutputStreamWriter outputStreamWriter = null;
+                        outputStreamWriter = new OutputStreamWriter(new FileOutputStream(miFichero));
+                        outputStreamWriter.write(String.valueOf(entradaTexto.getText()));
+                        outputStreamWriter.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
             }
         });
 
